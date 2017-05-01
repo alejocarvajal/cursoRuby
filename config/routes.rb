34ab35db3822d1125ca2af92392f7d4acd23c97e
alejo <alejo.carvajal03@gmail.com>
2017-05-01 14:06:34 -0500

    Actualizacion gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'sitio#inicio'
   get 'acerca' => 'sitio#acerca'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
